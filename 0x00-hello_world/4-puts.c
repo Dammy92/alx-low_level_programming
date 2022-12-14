d91b18ca91e237a25a685fa56942890e8b2abd52
Dammy92 <ajaladamilola@gmail.com>
2022-12-14 10:29:59 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+/**
+ * Main is void
+ * puts does not require new line command
+ * return is 0
+ */
 
 int main(void)
 
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle);
 
 	return (0);
 }
