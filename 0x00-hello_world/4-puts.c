0317ba71074ea21dbef59c511ae599fa33acb6ad
Dammy92 <ajaladamilola@gmail.com>
2022-12-14 09:55:19 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
-#include <stdio.hh>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void);
+
 {
-	char *s = "Programming is like building a multilingual puzzle";
+	char[100];
+
+	puts("Programming is like building a multilingual puzzle");
 
 	return (0);
 
