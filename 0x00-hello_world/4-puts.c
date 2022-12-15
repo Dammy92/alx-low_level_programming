f1f8d794f72e370686671912acb8216142ad1dc2
Dammy92 <ajaladamilola@gmail.com>
2022-12-14 16:16:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - prints phrase
- * return: is 0 if well executed
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	puts("Programing is like building a multilingual puzzle");
+
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
